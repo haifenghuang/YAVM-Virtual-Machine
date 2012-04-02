ed814909ae075806cc99c760432845e89bdce699
Cairn <cairnc@gmail.com>
2012-04-02 00:00:30 -0600

    Cleaned some stuff up

--- Source/vm.c
+++ Source/vm.c
@@ -22,16 +22,13 @@ static void cmp(void);
 static void lt(void);
 static void lte(void);
 static void jmp(void);
-static void mov(void);
 
+//memory
+static void mov(void);
 
 static void decode(void);
 
-
 static int checkType(int opType, int arg1, int arg2, int arg3);
-
-
-
 
 OPCODE opTable[] = {
 	&add,
@@ -50,8 +47,8 @@ OPCODE opTable[] = {
 
 static int * program;
 
-//variables declared in .data
-static Object * heap;
+//registers 
+static Object * reg;
 
 
 // program counter
@@ -64,12 +61,12 @@ static int indexC;
 static int indexD;
 
 
-int vm_init(int stackSz,int heapSz)
+int vm_init(int stackSz,int regSz)
 {
 
-	heap = (Object*)malloc(sizeof(Object) * heapSz);
+	reg = (Object*)malloc(sizeof(Object) * regSz);
 
-	if(heap == NULL)
+	if(reg == NULL)
 		return -1;
 
 	return 0;
@@ -77,7 +74,7 @@ int vm_init(int stackSz,int heapSz)
 }
 int vm_run(int * a_program)
 {
-	if(heap == NULL)
+	if(reg == NULL)
 		return 1;
 	if(a_program == NULL)
 		return 2;
@@ -105,48 +102,48 @@ int vm_run(int * a_program)
 
 int vm_shutDown()
 {
-	free(heap);
+	free(reg);
 }
 
 
 static void add(void)
 {
-	//add takes two numbers from top of stack adds them and places result on top of the stack
+	//add takes two values from designated registers and places result in another register
 	
-	heap[indexC].value.n = heap[indexA].value.n + heap[indexB].value.n;
+	reg[indexC].value.n = reg[indexA].value.n + reg[indexB].value.n;
 	return;
 }
 
 static void sub(void)
 {
-	//sub takes two values from accumulator subs them and places the result on the top of the stack
+	//sub takes two values from designated registers and places result in another register
 
-	heap[indexC].value.n = heap[indexA].value.n - heap[indexB].value.n;
+	reg[indexC].value.n = reg[indexA].value.n - reg[indexB].value.n;
 	return;
 }
 
 static void mul(void)
 {
-	//mul takes two values from accumulator multpclies them and places the result on the top of the stack
+	//mul takes two values from designated registers and places result in another register
 
-	heap[indexC].value.n = heap[indexA].value.n * heap[indexB].value.n;
+	reg[indexC].value.n = reg[indexA].value.n * reg[indexB].value.n;
 	return;
 }
 
 static void dvd(void)
 {
-	//dvd takes two values from accumulator divides them and places the result on the top of the stack
+	//dvd takes two values from designated registers and places result in another register
 
-	heap[indexC].value.n = heap[indexA].value.n / heap[indexB].value.n;
+	reg[indexC].value.n = reg[indexA].value.n / reg[indexB].value.n;
 	return;
 }
 
 static void mod(void)
 {
-	//mod takes two values from accumulator, casts them to int since they could be doubles, mods them 
-	//and then places the result on the top of the stack
+	//mod takes two values from designated registers, casts them to int since they could be doubles, mods them 
+	//and then places the result in another register
 
-	heap[indexC].value.n = (int)heap[indexA].value.n % (int)heap[indexB].value.n;
+	reg[indexC].value.n = (int)reg[indexA].value.n % (int)reg[indexB].value.n;
 	return;
 }
 
@@ -154,7 +151,7 @@ static void sqt(void)
 {
 	//sqt takes a value from the top of the stack takes the sqrt and places result back on top
 	
-	heap[indexC].value.n = sqrt(heap[indexA].value.n);
+	reg[indexC].value.n = sqrt(reg[indexA].value.n);
 	return;
 }
 
@@ -162,30 +159,30 @@ static void pwr(void)
 {
 	//pwr raises indexA to indexB and places the result on top of stack
 	
-	heap[indexC].value.n = pow(heap[indexA].value.n, heap[indexB].value.n);
+	reg[indexC].value.n = pow(reg[indexA].value.n, reg[indexB].value.n);
 	return;
 }
 
 static void cmp(void)//todo possibly make a register for this just to seperate the crap
 {
 	//TODO: possibly change this to just int's
-	if(&heap[indexA] == &heap[indexB])
+	if(&reg[indexA] == &reg[indexB])
 		return;
 
-	if(heap[indexA].type == heap[indexB].type)
+	if(reg[indexA].type == reg[indexB].type)
 	{
-		switch(heap[indexA].type)
+		switch(reg[indexA].type)
 		{
 		case T_NUM:
-			pc += (abs(heap[indexA].value.n - heap[indexB].value.n) < EPSILON) ? 0 : 1;
+			pc += (abs(reg[indexA].value.n - reg[indexB].value.n) < EPSILON) ? 0 : 1;
 			break;
 		case T_BLN:
-			pc += (heap[indexA].value.b == heap[indexB].value.b) ? 0 : 1;
+			pc += (reg[indexA].value.b == reg[indexB].value.b) ? 0 : 1;
 			break;
 		case T_STR:
-			if(heap[indexA].value.s->length <= 0)
+			if(reg[indexA].value.s->length <= 0)
 				break;
-			pc += strncmp(heap[indexA].value.s->string, heap[indexB].value.s->string, heap[indexA].value.s->length) ? 0 : 1;
+			pc += strncmp(reg[indexA].value.s->string, reg[indexB].value.s->string, reg[indexA].value.s->length) ? 0 : 1;
 			break;
 		}
 	}
@@ -195,14 +192,14 @@ static void cmp(void)//todo possibly make a register for this just to seperate t
 
 static void lt(void)
 {
-	pc += (heap[indexA].value.n < heap[indexB].value.n) && (fabs(heap[indexA].value.n - heap[indexB].value.n) > EPSILON) ? 0 : 1;
+	pc += (reg[indexA].value.n < reg[indexB].value.n) && (fabs(reg[indexA].value.n - reg[indexB].value.n) > EPSILON) ? 0 : 1;
 }
 
 static void lte(void)
 {
-	if(&heap[indexA] == &heap[indexB])
+	if(&reg[indexA] == &reg[indexB])
 		return;
-	pc += (heap[indexA].value.n < heap[indexB].value.n) || (fabs(heap[indexA].value.n - heap[indexB].value.n) < EPSILON) ? 0 : 1;
+	pc += (reg[indexA].value.n < reg[indexB].value.n) || (fabs(reg[indexA].value.n - reg[indexB].value.n) < EPSILON) ? 0 : 1;
 }
 
 static void jmp(void)
@@ -212,29 +209,29 @@ static void jmp(void)
 
 static void mov(void)
 {
-	heap[indexA] = heap[indexB];	
+	reg[indexA] = reg[indexB];	
 }
 
 static void decode(void)
 {
 	//decode opcode into respective parts such as indexes for data etc.
 
-	int instruction = *pc++;
+	pc++;
 
-	opType = instruction & 0x000000FF;
+	opType = *pc & 0x000000FF;
 
-	indexA = (instruction >> 8 ) & 0xFF;
-	indexB = (instruction >> 16) & 0xFF;
-	indexC = (instruction >> 24) & 0xFF;
-	indexD = (instruction >> 8);
+	indexA = (*pc >> 8 ) & 0xFF;
+	indexB = (*pc >> 16) & 0xFF;
+	indexC = (*pc >> 24) & 0xFF;
+	indexD = (*pc >> 8);
 	
 	//TODO: possibly convert to inline
 	if( opArgs[opType].check == 3 )
-		checkType(opType, heap[indexC].type, heap[indexA].type, heap[indexB].type);
+		checkType(opType, reg[indexC].type, reg[indexA].type, reg[indexB].type);
 	else if (opArgs[opType].check == 2)
-		checkType(opType, heap[indexA].type, heap[indexB].type, T_NONE);
+		checkType(opType, reg[indexA].type, reg[indexB].type, T_NONE);
 	else if (opArgs[opType].check == 1)
-		checkType(opType, heap[indexA].type, T_NONE, T_NONE);
+		checkType(opType, reg[indexA].type, T_NONE, T_NONE);
 	                                               
 }
 
